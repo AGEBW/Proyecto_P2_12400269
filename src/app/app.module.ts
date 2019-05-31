f7c61f93a4ec4754a139f8eb7ae478a0aaf37e95
AGEBW <a.d.river@hotmail.com>
2019-05-30 23:14:46 -0600

    concluido el P2 pero con problema de interfaz producto por imagenes

---
+++ src/app/app.module.ts
@@ -0,0 +1,54 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+import { Product } from './product';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import {
+  MatInputModule,
+  MatPaginatorModule,
+  MatProgressSpinnerModule,
+  MatSortModule,
+  MatTableModule,
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule } from '@angular/material';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductsComponent,
+    ProductDetailComponent,
+    ProductAddComponent,
+    ProductEditComponent
+  ],
+  imports: [
+    BrowserModule,
+  FormsModule,
+  HttpClientModule,
+  AppRoutingModule,
+  ReactiveFormsModule,
+  BrowserAnimationsModule,
+  MatInputModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatProgressSpinnerModule,
+  MatIconModule,
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/product.ts
@@ -0,0 +1,14 @@
+export class Product {
+    // tslint:disable-next-line:variable-name
+    _id: string;
+    // tslint:disable-next-line:variable-name
+    prod_name: string;
+    // tslint:disable-next-line:variable-name
+    prod_desc: string;
+    // tslint:disable-next-line:variable-name
+    prod_price: number;
+    // tslint:disable-next-line:variable-name
+    updated_at: Date;
+    // tslint:disable-next-line:variable-name
+    prod_image: string;
+  }
